d0921988516f267765de75ff86ca88622f2a3a0a
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-06-20 16:05:37 +0530

    create Header/Footer

--- frontend/src/components/common/Layout.jsx
+++ frontend/src/components/common/Layout.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 
-const Layout = ({children}) => {
-  return (
-    <>
-    <Header/>
-    {children}
-    <Footer/>
-    </>
-  )
+const Layout = ({ children }) => {
+    return (
+        <>
+            <Header />
+            {children}
+            <Footer />
+        </>
+    )
 }
 
 export default Layout
